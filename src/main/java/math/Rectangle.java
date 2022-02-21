bbc87fec6ff3a3670ec782807787333dad252e99
Nahuel Verdugo Revigliono <nahuelvr@gmail.com>
2022-02-21 12:44:51 +0100

    feat: add perimeter calculation

--- src/main/java/math/Rectangle.java
+++ src/main/java/math/Rectangle.java
@@ -1,4 +1,16 @@
 package math;
 
 public class Rectangle {
+
+    private double height;
+    private double width;
+
+    public Rectangle(double x, double y){
+        this.height = x;
+        this.width = y;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (height + width);
+    }
 }
--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -3,15 +3,20 @@ package oop;
 import math.Rectangle;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class RectangleTest {
 
     @Test
     public void itShouldInstanceARectangle() {
-
-        Rectangle rectangle = new Rectangle();
-
+        Rectangle rectangle = new Rectangle(0,0);
         assertNotNull(rectangle);
+    }
+
+    @Test
+    public void itShouldCalculatePerimeter() {
+        Rectangle rectangle = new Rectangle(2.5, 2.5);
+        assertEquals(rectangle.calculatePerimeter(), 10);
     }
 }

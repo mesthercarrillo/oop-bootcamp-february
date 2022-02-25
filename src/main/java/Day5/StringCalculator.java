a94db87d508399ea6510bc55926e3a291f31b2a8
Esther Carrillo <esther.carrillo@thoughtworks.com>
2022-02-25 12:41:57 +0100

    [CSR/EC] StringCalculator refactor

--- src/main/java/Day5/StringCalculator.java
+++ src/main/java/Day5/StringCalculator.java
@@ -4,16 +4,15 @@ public class StringCalculator {
 
 
     public static int add(String numbers) {
-
         if (numbers.length() == 0) {
             return 0;
         } else if (numbers.length() == 1) {
-            return Integer.valueOf(numbers);
+            return Integer.parseInt(numbers);
         } else {
             String[] splitNumbers = numbers.split(",");
             int sum = 0;
-            for (int i = 0; i < splitNumbers.length; i++) {
-                sum = sum + Integer.valueOf(splitNumbers[i]);
+            for (String splitNumber : splitNumbers) {
+                sum = sum + Integer.parseInt(splitNumber);
             }
             return sum;
         }
